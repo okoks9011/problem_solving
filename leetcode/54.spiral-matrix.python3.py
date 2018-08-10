292f5783de51f3ce2ac9eda7f1ddcd3a86d09b29
eazy <okoks9011@gmail.com>
2018-08-10 16:27:27 +0900

    Enhance leetcode 54

--- leetcode/54.spiral-matrix.python3.py
+++ leetcode/54.spiral-matrix.python3.py
@@ -21,17 +21,13 @@ class Solution:
                 result.extend(matrix[i][j:j_last+1])
                 break
             elif m == 1:
-                for r in range(i, i_last+1):
-                    result.append(matrix[r][j])
+                result.extend(matrix[r][j] for r in range(i, i_last+1))
                 break
 
             result.extend(matrix[i][j:j_last])
-            for r in range(i, i_last):
-                result.append(matrix[r][j_last])
-            for c in range(j_last, j, -1):
-                result.append(matrix[i_last][c])
-            for r in range(i_last, i, -1):
-                result.append(matrix[r][j])
+            result.extend(matrix[r][j_last] for r in range(i, i_last))
+            result.extend(matrix[i_last][c] for c in range(j_last, j, -1))
+            result.extend(matrix[r][j] for r in range(i_last, i, -1))
 
             i, j = i + 1, j + 1
             n, m = n - 2, m - 2
