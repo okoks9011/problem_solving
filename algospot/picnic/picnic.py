8f4c9d423c34a187e263270315e72d675dce122a
eazy <okoks9011@gmail.com>
2018-09-11 12:34:57 +0900

    Add picnic

---
+++ algospot/picnic/picnic.py
@@ -0,0 +1,31 @@
+def picnic_pick(sofar, remain, candi):
+    if remain == 0:
+        return 1
+    if not candi:
+        return 0
+    result = 0
+    for i in range(len(candi)):
+        c1, c2 = candi[i]
+        if (c1 not in sofar) and (c2 not in sofar):
+            sofar.add(c1)
+            sofar.add(c2)
+            result += picnic_pick(sofar, remain-1, candi[i+1:])
+            sofar.remove(c1)
+            sofar.remove(c2)
+    return result
+
+def picnic(n, m, pairs):
+    return picnic_pick(set(), n//2, pairs)
+
+def main():
+    c = int(input())
+    cases = []
+    for _ in range(c):
+        n, m = [int(s) for s in input().split()]
+        raw_pairs = [int(s) for s in input().split()]
+        pairs = []
+        for i in range(len(raw_pairs)//2):
+            pairs.append((raw_pairs[2*i], raw_pairs[2*i+1]))
+        print(picnic(n, m, pairs))
+
+main()
