9a9f653446834f025b901a650810ad6364b348a0
eazy <okoks9011@gmail.com>
2018-10-02 14:41:29 +0900

    Enhance 126 in leetcode, still TLE

--- leetcode/126.word-ladder-ii.python3.py
+++ leetcode/126.word-ladder-ii.python3.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from collections import deque
 
-Triple = namedtuple('Triple', ['word', 'candi', 'history'])
+Triple = namedtuple('Triple', ['word', 'history'])
 
 def adjcent_word(A, B):
     return 1 == sum(1 for a, b in zip(A,B) if a != b)
@@ -14,7 +14,8 @@ class Solution:
         :type wordList: List[str]
         :rtype: List[List[str]]
         """
-        s = Triple(beginWord, set(wordList), [beginWord])
+        s = Triple(beginWord, [beginWord])
+        candi = set(wordList)
         cur_q = deque()
         next_q = deque()
         cur_q.append(s)
@@ -24,12 +25,13 @@ class Solution:
                 return list(map(lambda x: x.history, target))
             while cur_q:
                 t = cur_q.popleft()
-                for c_word in t.candi:
+                for c_word in candi:
                     if adjcent_word(t.word, c_word):
-                        new_candi = t.candi.copy()
-                        new_candi.remove(c_word)
                         new_history = t.history.copy()
                         new_history.append(c_word)
-                        next_q.append(Triple(c_word, new_candi, new_history))
+                        next_q.append(Triple(c_word, new_history))
+            for i in next_q:
+                if i.word in candi:
+                    candi.remove(i.word)
             cur_q, next_q = next_q, cur_q
         return []
