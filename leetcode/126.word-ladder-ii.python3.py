24c398446c1b5521973c5b3dcdca736bbda8c379
eazy <okoks9011@gmail.com>
2018-10-03 11:25:54 +0900

    Solve 126 in leetcode

--- leetcode/126.word-ladder-ii.python3.py
+++ leetcode/126.word-ladder-ii.python3.py
@@ -1,19 +1,9 @@
-from collections import namedtuple
-from collections import deque
+import string
 
-def gen_all_path(total, beginWord, endWord):
-    if beginWord == endWord:
-        return [[beginWord]]
-    result = []
-    for e in total[endWord]:
-        e_paths = gen_all_path(total, beginWord, e)
-        for e_path in e_paths:
-            e_path.append(endWord)
-            result.append(e_path)
-    return result
-
-def adjcent_word(A, B):
-    return 1 == sum(1 for a, b in zip(A,B) if a != b)
+def gen_adjcent(word):
+    for i in range(len(word)):
+        for c in string.ascii_lowercase:
+            yield word[:i] + c + word[i+1:]
 
 class Solution:
     def findLadders(self, beginWord, endWord, wordList):
@@ -26,21 +16,19 @@ class Solution:
         candi = set(wordList)
         cur = {}
         nextt = {}
-        total = {}
-        cur[beginWord] = None
+        cur[beginWord] = [[beginWord]]
         while cur:
             if endWord in cur:
-                 return gen_all_path(total, beginWord, endWord)
+                 return cur[endWord]
             for t_word in cur:
-                for c_word in candi:
-                    if adjcent_word(t_word, c_word):
+                for c_word in gen_adjcent(t_word):
+                    if c_word in candi:
                         if c_word not in nextt:
                             nextt[c_word] = []
-                        nextt[c_word].append(t_word)
+                        nextt[c_word].extend(t_path + [c_word] for t_path in cur[t_word])
             for i_word in nextt:
                 if i_word in candi:
                     candi.remove(i_word)
-            total.update(nextt)
             cur = nextt
             nextt = {}
         return []
