1162c2e54895b4b2471959ef32508d7a91f627e5
eazy <okoks9011@gmail.com>
2018-12-03 09:23:35 +0900

    enhance codeforces edu round 54

--- codeforces/edu_round_54/divisor.cc
+++ codeforces/edu_round_54/divisor.cc
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 using std::cin;
 using std::cout;
 using std::endl;
+
+using std::sqrt;
 
 int main() {
     long long n;
@@ -14,9 +17,13 @@ int main() {
     }
 
     long long p = 3;
-    for (p; p <= n; p += 2) {
+    long long sqrt_n = sqrt(n);
+    for (; p <= sqrt_n; p += 2) {
         if (n % p == 0)
             break;
     }
-    cout << 1 + (n - p) / 2 << endl;
+    if (p > sqrt_n)
+        cout << 1 << endl;
+    else
+        cout << 1 + (n - p) / 2 << endl;
 }
