5c8794183a4a88f011d31dad49ec0c5869ed0c63
eazy <okoks9011@gmail.com>
2019-01-23 17:01:52 +0900

    Enhance codeforces round 531 balanced ternary string

---
+++ codeforces/round_531/ternary3.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+int CalNeed(const vector<int>& cnt, const vector<int>& goal) {
+    int total = 0;
+    for(int i = 0; i < cnt.size(); ++i) {
+        total += abs(cnt[i] - goal[i]);
+    }
+
+    return total / 2;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    vector<int> cnt(3);
+    for (int i = 0; i < 3; ++i)
+        cnt[i] = count(s.begin(), s.end(), '0'+i);
+    vector<int> goal(3, n / 3);
+
+    int remain = CalNeed(cnt, goal);
+    int i = 0;
+    while (remain) {
+        int si = s[i] - '0';
+        --cnt[si];
+        for (int j = 0; j < 3; ++j) {
+            if (goal[j] <= 0)
+                continue;
+
+            --goal[j];
+            if (CalNeed(cnt, goal) + (si != j) == remain) {
+                s[i] = j + '0';
+                remain -= (si != j);
+                break;
+            }
+            ++goal[j];
+        }
+        ++i;
+    }
+    cout << s << endl;
+}
