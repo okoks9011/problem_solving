db61c59c16ba027a9ca2aa86f47cbf55d82d72f3
eazy <okoks9011@gmail.com>
2019-02-09 01:32:47 +0900

    Enhance codeforces round 525 D. Diverse Garland

--- codeforces/round_535/diverse_garland.cc
+++ codeforces/round_535/diverse_garland.cc
@@ -14,26 +14,17 @@ int main() {
     string s;
     cin >> s;
 
-    vector<char> c{'R', 'G', 'B'};
+    string c("RGB");
     int cnt = 0;
-    for (int i = 1; i < n-1; ++i) {
+    for (int i = 1; i < n; ++i) {
         if (s[i-1] == s[i]) {
             for (int j = 0; j < 3; ++j) {
-                if (c[j] != s[i-1] && c[j] != s[i+1]) {
+                if (c[j] != s[i-1] &&
+                    (i == n-1 || c[j] != s[i+1])) {
                     s[i] = c[j];
                     ++cnt;
                     break;
                 }
-            }
-        }
-    }
-
-    if (s[s.size()-1] == s[s.size()-2]) {
-        for (int j = 0; j < 3; ++j) {
-            if (s[s.size()-2] != c[j]) {
-                s[s.size()-1] = c[j];
-                ++cnt;
-                break;
             }
         }
     }
