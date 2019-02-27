0309476f5fbb9171ddbb66cef33071d3820447e0
eazy <okoks9011@gmail.com>
2019-02-27 20:28:32 +0900

    Enhance codeforces round 540 A. water

--- codeforces/round_540/water.cc
+++ codeforces/round_540/water.cc
@@ -1,22 +1,17 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
-
-void Solve() {
-    long long n;
-    int a, b;
-    cin >> n >> a >> b;
-
-    if (2 * a <= b)
-        cout << n * a << endl;
-    else
-        cout << (n / 2) * b + (n % 2) * a << endl;
-}
 
 int main() {
     int q;
     cin >> q;
 
-    for (int i = 0; i < q; ++i)
-        Solve();
+    for (int i = 0; i < q; ++i) {
+        long long n;
+        int a, b;
+        cin >> n >> a >> b;
+
+        cout << (n / 2) * min(2*a, b) + (n % 2) * a << endl;
+    }
 }
