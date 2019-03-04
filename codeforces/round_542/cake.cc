440c31429e4df062e138e47b6e8af88a43833371
eazy <okoks9011@gmail.com>
2019-03-04 23:10:01 +0900

    Enhance codeforces round 542 B

--- codeforces/round_542/cake.cc
+++ codeforces/round_542/cake.cc
@@ -13,21 +13,19 @@ int main() {
     cin >> n;
 
     int m = 2*n;
-    vector<vector<int>> cnt(n+1);
+    vector<vector<int>> loc(n+1);
     for (int i = 0; i < m; ++i) {
         int tmp;
         cin >> tmp;
-        cnt[tmp].emplace_back(i);
+        loc[tmp].emplace_back(i);
     }
-    cnt[0] = {0, 0};
+    loc[0] = {0, 0};
 
     long long result = 0;
     for (int i = 1; i <= n; ++i) {
-        auto& from = cnt[i-1];
-        auto& to = cnt[i];
-        int result1 = abs(from[0] - to[0]) + abs(from[1] - to[1]);
-        int result2 = abs(from[0] - to[1]) + abs(from[1] - to[0]);
-        result += min(result1, result2);
+        auto& from = loc[i-1];
+        auto& to = loc[i];
+        result += abs(from[0] - to[0]) + abs(from[1] - to[1]);
     }
     cout << result << endl;
 }
