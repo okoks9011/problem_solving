50f1370fe2f3a30da878849dd9fdfab34faae716
eazy <okoks9011@gmail.com>
2019-03-25 00:08:21 +0900

    Enhance coin in codeforces round 523

---
+++ codeforces/round_523/coins2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+using std::vector;
+
+int main() {
+    int n, s;
+    cin >> n >> s;
+    cout << (s+n-1) / n << endl;
+}
