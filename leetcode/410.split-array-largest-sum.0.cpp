7f15035f5ca614a5e423cc775cd81493f7471f14
eazy <okoks9011@gmail.com>
2019-05-14 21:42:02 +0900

    Enhance leetcode 410 split array largest sum

---
+++ leetcode/410.split-array-largest-sum.0.cpp
@@ -0,0 +1,48 @@
+#include <numeric>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool SplitWith(const vector<int>& nums, int m, long long max_sum) {
+        int cnt = 1;
+        long long sum = 0;
+        for (auto& ni : nums) {
+            if (ni > max_sum)
+                return false;
+
+            if (sum + ni <= max_sum) {
+                sum += ni;
+            } else {
+                sum = ni;
+                ++cnt;
+            }
+
+            if (cnt > m)
+                return false;
+        }
+        return true;
+    }
+
+    int splitArray(vector<int>& nums, int m) {
+        long long left = *min_element(nums.begin(), nums.end());
+        long long right = 0;
+        for (auto& ni : nums)
+            right += ni;
+
+        long long result = -1;
+        while (left <= right) {
+            auto mid = left + (right-left) / 2;
+            if (SplitWith(nums, m, mid)) {
+                right = mid - 1;
+                result = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return static_cast<int>(result);
+    }
+};
