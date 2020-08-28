ffdb5fc484370535c9e95e2671f38ce9b13f919c
eazy <okoks9011@gmail.com>
2020-08-28 21:42:02 +0900

    Add codeforces problem

---
+++ codeforces/personal_round_09W1/chess.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 2) {
+        cout << "black" << endl;
+    } else {
+        cout << "white" << endl;
+        cout << "1 2" << endl;
+    }
+}
