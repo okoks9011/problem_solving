2559d8b2b91502cbe3a7f21fa16c735d33dd8267
eazy <okoks9011@gmail.com>
2020-11-06 00:08:55 +0900

    Add leetcode

--- leetcode/1140.cc
+++ leetcode/1140.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 class Solution {
   public:
-    int CalMax(int cur_i, int cur_m, int cur_z, vector<vector<vector<int>>>* dp_ptr,
+    int CalMax(int cur_i, int cur_m, vector<vector<int>>* dp_ptr,
                const vector<int>& suf) {
         auto& dp = *dp_ptr;
         int n = dp.size();
@@ -16,22 +16,21 @@ class Solution {
         if (cur_i + 2 * cur_m >= n)
             return suf[cur_i];
 
-        auto& result = dp[cur_i][cur_m][cur_z];
+        auto& result = dp[cur_i][cur_m];
         if (result != -1)
             return result;
-        int new_z = cur_z ? 0 : 1;
         for (int x = 1; x <= cur_m * 2; ++x)
-            result = max(result, suf[cur_i] - CalMax(cur_i + x, max(x, cur_m), new_z, dp_ptr, suf));
+            result = max(result, suf[cur_i] - CalMax(cur_i + x, max(x, cur_m), dp_ptr, suf));
         return result;
     }
 
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(51, vector<int>(2, -1)));
+        vector<vector<int>> dp(n, vector<int>(51, -1));
 
         vector<int> suf(n+1);
         for (int i = n-1; i >= 0; --i)
             suf[i] = suf[i+1] + piles[i];
-        return CalMax(0, 1, 0, &dp, suf);
+        return CalMax(0, 1, &dp, suf);
     }
 };
