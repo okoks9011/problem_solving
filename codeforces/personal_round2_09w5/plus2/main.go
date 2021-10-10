a48fd63eadb2f6dde91cc9abd1371f06accf8e26
eazy <okoks9011@gmail.com>
2021-10-10 11:24:46 +0900

    Add files

---
+++ codeforces/personal_round2_09w5/plus2/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var in *bufio.Reader
+
+func solve() {
+	var n, a, b int64
+	fmt.Fscan(in, &n)
+	fmt.Fscan(in, &a)
+	fmt.Fscan(in, &b)
+
+	var x int64 = 1
+	for x < n {
+		if x%b == n%b {
+			fmt.Println("Yes")
+			return
+		}
+		x *= a
+	}
+	fmt.Println("No")
+}
+
+func main() {
+	in = bufio.NewReader(os.Stdin)
+
+	var t int
+	fmt.Fscan(in, &t)
+
+	for i := 0; i < t; i++ {
+		solve()
+	}
+}
